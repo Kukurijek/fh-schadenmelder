e9f64b33249881b9f14611ad3275c79ac6fefaa2
Nemanja Filipovic <a01348905@unet.univie.ac.at>
2019-11-16 14:55:34 +0100

    add new Controller for the camera

---
+++ CameraController/CameraController.swift
@@ -0,0 +1,24 @@
+//
+//  CameraController.swift
+//  FH Schadenmelder
+//
+//  Created by Nemanja Filipovic on 16.11.19.
+//  Copyright © 2019 Nemanja Filipovic, Michael Prammer, Elmi  Abdullahi. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CameraController : UIViewController {
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
