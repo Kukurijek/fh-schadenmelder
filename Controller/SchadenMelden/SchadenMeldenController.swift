95e34e425d5d149315cb18704cf1d6c4d5790420
Nemanja Filipovic <a01348905@unet.univie.ac.at>
2019-11-17 18:59:01 +0100

    add new viewController, do not let user to make entry without details about place and category

---
+++ Controller/SchadenMelden/SchadenMeldenController.swift
@@ -0,0 +1,64 @@
+//
+//  SchadenMeldenController.swift
+//  FH Schadenmelder
+//
+//  Created by Nemanja Filipovic on 16.11.19.
+//  Copyright © 2019 Nemanja Filipovic, Michael Prammer, Elmi  Abdullahi. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import FirebaseDatabase
+
+class SchadenMeldenController : UIViewController {
+
+    @IBOutlet weak var schadenArt: UITextField!
+    @IBOutlet weak var schadenOrt: UITextField!
+    @IBOutlet weak var sendButton: UIButton!
+
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func uploadNewDamageEntryToDatabase(image: String) {
+        let databaseRef = Database.database().reference().child("Einträge")
+        
+    }
+    
+    @IBAction func addPhotoButton(_ sender: Any) {
+        //sendButton.isEnabled = false
+        print("test")
+    }
+    @IBAction func sendButton(_ sender: Any) {
+        print("senddd")
+        uploadNewDamageEntryToDatabase(image: "lele")
+    }
+    
+    func addTargetToTheTextField() {
+        schadenArt.addTarget(self, action: #selector(textFieldDidChange), for: UIControl.Event.editingChanged)
+        schadenOrt.addTarget(self, action: #selector(textFieldDidChange), for: UIControl.Event.editingChanged)
+    }
+    
+    @objc func textFieldDidChange(){
+        let isText = schadenArt.text?.count ?? 0 > 0 && schadenOrt.text?.count ?? 0 > 0
+        
+        if isText {
+            sendButton.isHidden = false
+        } else {
+            sendButton.isHidden = true
+        }
+    }
+    
+    override func viewDidLoad() {
+        addTargetToTheTextField()
+        sendButton.isHidden = true
+    }
+}
--- Podfile
+++ Podfile
@@ -8,6 +8,7 @@ target 'FH Schadenmelder' do
 # add the Firebase pod for Google Analytics
 pod 'Firebase/Analytics'
 pod 'FirebaseAuth'
+pod 'Firebase/Database'
 pod 'ProgressHUD'
 # add pods for any other desired Firebase products
 # https://firebase.google.com/docs/ios/setup#available-pods
