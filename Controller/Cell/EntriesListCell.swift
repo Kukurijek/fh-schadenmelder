89d7cfc8a24af8e04467d957a1e7c6eee6ff5bdd
Nemanja Filipovic <filipovic@youniqx.comc>
2019-12-21 14:27:19 +0100

    add custom table cell

---
+++ Controller/Cell/EntriesListCell.swift
@@ -0,0 +1,26 @@
+//
+//  EntriesListCell.swift
+//  FH Schadenmelder
+//
+//  Created by Filipovic Nemanja on 21.12.19.
+//  Copyright © 2019 Nemanja Filipovic, Michael Prammer, Elmi  Abdullahi. All rights reserved.
+//
+
+import UIKit
+
+class EntriesListCell: UITableViewCell {
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var date: UILabel!
+    /*
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+*/
+}
--- Controller/ListeController.swift
+++ Controller/ListeController.swift
@@ -8,23 +8,36 @@
 
 import UIKit
 
-class ListeController: UIViewController {
+class ListeController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+
+    @IBOutlet weak var table: UITableView!
+    
+    var entries: [String] = []
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        table.delegate = self
+        table.dataSource = self
 
-        // Do any additional setup after loading the view.
     }
     
 
-    /*
-    // MARK: - Navigation
+     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
+     }
+     
+     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = table.dequeueReusableCell(withIdentifier: "EntriesListCellIdentifier", for: indexPath) as? EntriesListCell else { fatalError() }
+        
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+        cell.textLabel?.text = "TEST"
+        
+     return cell
+     }
+     
+
 
 }
