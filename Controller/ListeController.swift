51547afe2920664ecbce45f977eff7e0b68e2d52
Nemanja Filipovic <a01348905@unet.univie.ac.at>
2020-01-09 21:11:22 +0100

    add fullsize image view

--- Controller/ListeController.swift
+++ Controller/ListeController.swift
@@ -15,7 +15,7 @@ class ListeController: UIViewController, UITableViewDelegate, UITableViewDataSou
 
 
     @IBOutlet weak var table: UITableView!
-    
+        
     var entries = [EntryModel]()
     
     override var prefersStatusBarHidden: Bool {
@@ -27,6 +27,7 @@ class ListeController: UIViewController, UITableViewDelegate, UITableViewDataSou
         table.delegate = self
         table.dataSource = self
         loadEntries()
+            
     }
 
      func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -40,7 +41,7 @@ class ListeController: UIViewController, UITableViewDelegate, UITableViewDataSou
         cell.time.text = entries[indexPath.row].time
         cell.category.text = entries[indexPath.row].damagePlace
         cell.title.text = entries[indexPath.row].damageType
-        var urlString: String = self.entries[indexPath.row].photo ?? "https://firebasestorage.googleapis.com/v0/b/fh-schadenmelder.appspot.com/o/Eintrag_Fotos%2F31CD244F-BD06-4AF2-94D8-6486BD5C441E?alt=media&token=f32325cd-6b7f-4542-a797-1958036592b5"
+        let urlString: String = self.entries[indexPath.row].photo ?? "https://firebasestorage.googleapis.com/v0/b/fh-schadenmelder.appspot.com/o/Eintrag_Fotos%2F31CD244F-BD06-4AF2-94D8-6486BD5C441E?alt=media&token=f32325cd-6b7f-4542-a797-1958036592b5"
         guard let url = URL(string: urlString) else { fatalError() }
         cell.icon.sd_setImage(with: url ) { (_, _, _, _) in
         }
@@ -49,6 +50,8 @@ class ListeController: UIViewController, UITableViewDelegate, UITableViewDataSou
         
         return cell
      }
+    
+
      
     func loadEntries() {
         let refDatabase = Database.database().reference().child("Eintragae")
@@ -61,6 +64,33 @@ class ListeController: UIViewController, UITableViewDelegate, UITableViewDataSou
         }
         print(entries)
     }
+    
+//    func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+//        let cell = table.cellForRow(at: indexPath) as! EntriesListCell
+//        self.addIm
+//    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let cell = tableView.cellForRow(at: indexPath) as! EntriesListCell
+        self.imageTapped(image: cell.icon.image!)
+    }
+    
+    func imageTapped(image:UIImage){
+        let newImageView = UIImageView(image: image)
+        newImageView.frame = UIScreen.main.bounds
+        newImageView.backgroundColor = .black
+        newImageView.contentMode = .scaleAspectFit
+        newImageView.isUserInteractionEnabled = true
+        let tap = UITapGestureRecognizer(target: self, action: #selector(ShowImageFullSizeController.dismissFullscreenImage(_:)))
+        newImageView.addGestureRecognizer(tap)
+        self.view.addSubview(newImageView)
+        self.navigationController?.isNavigationBarHidden = true
+        self.tabBarController?.tabBar.isHidden = true
+    }
 
-
+    @objc func dismissFullscreenImage(_ sender: UITapGestureRecognizer) {
+        self.navigationController?.isNavigationBarHidden = false
+        self.tabBarController?.tabBar.isHidden = false
+        sender.view?.removeFromSuperview()
+    }
 }
---
+++ Controller/ShowImageFullSizeController.swift
@@ -0,0 +1,25 @@
+//
+//  ShowImageFullSizeController.swift
+//  FH Schadenmelder
+//
+//  Created by Nemanja Filipovic on 09.01.20.
+//  Copyright © 2020 Nemanja Filipovic, Michael Prammer, Elmi  Abdullahi. All rights reserved.
+//
+
+import UIKit
+
+class ShowImageFullSizeController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @objc func dismissFullscreenImage(_ sender: UITapGestureRecognizer) {
+        self.navigationController?.isNavigationBarHidden = false
+        self.tabBarController?.tabBar.isHidden = false
+        sender.view?.removeFromSuperview()
+    }
+
+}
